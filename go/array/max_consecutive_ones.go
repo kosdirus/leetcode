d14abe69921e130c4bba15b1c4a3e7c738295825
Ivan <kos.dirus@gmail.com>
2022-05-19 23:10:35 +0200

    Go solution: Array - Max Consecutive Ones

---
+++ go/array/max_consecutive_ones.go
@@ -0,0 +1,39 @@
+package array
+
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+	Input: nums = [1,1,0,1,1,1]
+	Output: 3
+	Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+Example 2:
+	Input: nums = [1,0,1,1,0,1]
+	Output: 2
+
+Constraints:
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
+
+// Returns the maximum number of consecutive 1's in the array
+func findMaxConsecutiveOnes(nums []int) int {
+	var n, res int
+	for i := range nums {
+		if nums[i] == 1 {
+			n++
+		} else {
+			if n > res {
+				res = n
+			}
+			n = 0
+		}
+	}
+
+	if n > res {
+		res = n
+	}
+
+	return res
+}
